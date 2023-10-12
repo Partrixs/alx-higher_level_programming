929d51f06aa73496efb5621dae7c4fd6721598e2
Partrixs <patricknjoroge092@gmail.com>
2023-10-12 15:48:27 +0300

    0. Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
+	int sum;
 
-	count = 0;
+	sum = 0;
 
 	if (h == NULL)
-		return (count);
+		return (sum);
 
 	while (h->prev != NULL)
 		h = h->prev;
@@ -22,9 +22,9 @@ size_t print_dlistint(const dlistint_t *h)
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		count++;
+		sum++;
 		h = h->next;
 	}
 
-	return (count);
+	return (sum);
 }
