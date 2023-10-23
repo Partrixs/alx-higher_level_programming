889ce4e631e66a011a55cd1238bb92962dc19bd0
Partrixs <patricknjoroge092@gmail.com>
2023-10-23 18:21:31 +0300

    9. ByteCode -> Python #4

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/bin/bash/python3
+def magic_calculation(a, b):
+    result = 0
+    for it in range(1, 3):
+        try:
+            if it > a:
+                raise Exception:
+            result += b + a
+        except Exception:
+            break
+    return result
